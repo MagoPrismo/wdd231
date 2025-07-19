c84c586de5257c95c887922f51beeac68ca09c7f
MagoPrismo <sgngms3@gmail.com>
2025-07-18 23:41:11 -0300

    mobile view

--- scripts/date.js
+++ scripts/date.js
@@ -4,3 +4,21 @@ const yearSpan = document.querySelector("#year");
 yearSpan.textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent = document.lastModified;
+
+//code for the hamburguer menu
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#hidden-menu');
+
+hambutton.addEventListener('click', () =>
+{
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
+const screenSize = window.innerWidth;
+
+if (screenSize <= 500) {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+}
